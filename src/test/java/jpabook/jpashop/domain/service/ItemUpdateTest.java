7518064b1717610cb5ec6df13feb689caa5e4a73
chaehyunbyung <gusqud87@naver.com>
2021-01-12 21:21:44 +0900

    완강 - 에러있음

---
+++ src/test/java/jpabook/jpashop/domain/service/ItemUpdateTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Test
+    void updateTest() throws Exception {
+
+    }
+}
